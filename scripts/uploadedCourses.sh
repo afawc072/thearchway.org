f56e6d92f0413531de2844aa8333b05c7c58c040
afawc072 <afawc072@uottawa.ca>
2015-02-16 18:53:20 -0500

    Update uploadedCourses.sh
    Added a filecount for every type of files

--- scripts/uploadedCourses.sh
+++ scripts/uploadedCourses.sh
@@ -10,9 +10,21 @@ dirList=$(du -hs)
 
 date=$(date -I) 
 
+ppt=$(locate -c *.ppt)
+
+docx=$(locate -c *.docx)
+
+doc=$(locate -c *.doc)
+
+pdf=$(locate -c *.pdf)
+
+odt=$(locate -c *.odt)
+
+
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!">"$path"
 echo "Statistics Courses with Uploaded Content $date">>"$path"
 echo "There are $numCourses courses containing files!">>"$path"
+echo "$ppt Powerpoint, $docx DocX files, $doc Doc files, $pdf PDF files and $odt OpenOffie/LibreOffice files!">>"$path"
 echo "Total Size: $dirList ">>"$path"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!">>"$path"
 
