48755a103863a8b334e319ee7c8f7cdf63132f73
pboud <pboud071@uottawa.ca>
2015-02-23 23:06:24 -0500

    Help.php - 3 steps

--- about.php
+++ about.php
@@ -1,37 +1,114 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>The Archway</title>
-<link rel="stylesheet" href="/style.1.css" type="text/css" />
-</head>
-<body>
-<div class="top">
-<h1><a>The Archway</a></h1>
-</div>
-<div class="footer">
+    <html style="">
 
-        <div id="content">
+    <head>
 
-            <h1>Help</h1>
+<?php session_start() ?>
+    <link href="css/metro-bootstrap.css" rel="stylesheet">
+    <link href="css/metro-bootstrap-responsive.css" rel="stylesheet">
+    <link href="iconFont.css" rel="stylesheet">
+    <link href="docs.css" rel="stylesheet">
+    <link href="prettify.css" rel="stylesheet">
 
-            <p>
-                Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
-            </p>
+    <script src="jquery.min.js"></script>
+    <script src="jquery.widget.min.js"></script>
+    <script src="jquery.mousewheel.js"></script>
+    <script src="prettify.js"></script>
 
-            <p>
-                Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
-            </p>
+    <script src="load-metro.js"></script>
 
+    <script src="docs.js"></script>
+    <script src="github.info.js"></script>
+    <script src="start-screen.js"></script>
 
-        </div>
+    </head>
+
+    <body class="metro">
+
+<script></script>
+
+<noscript></noscript>
+
+<div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
+
+<?php include ("templates/header.php"); ?>
+
+    <div id="isc_3" class="normal" onscroll="return isc_VLayout_0._handleCSSScroll()" style="position: relative;width: 100%; top: 10%; margin: 0 auto;">
         
-<p class="generic" style="font-size: 12px;"><a href="index.html">Search</a> <a href="profile.html">Profile</a> <a href="about.php">Help</a> | <a href="upload.php"> Upload | </a> <a href="logout.php"> Logout  <br/><br/></a></p>
-&copy; 2014 The Archway</div>
+                 
+               
+    
+    <div id="isc_4" style="POSITION:relative;display:inline-block;-moz-box-sizing:borde…-align:top;VISIBILITY:inherit;Z-INDEX:200072;CURSOR:default;" >
+        <div id="isc_5" class="normal" style="position: absolute; left: 0px; top: 0px; width: 779px; heigh…; box-sizing: border-box; cursor: default;" >
 
-<script type="text/javascript">
-$(document).ready(function() {
-	$("#thesearchbox").focus();
-});
-</script>
-</body>
-</html>
+       </div>
+
+</div>
+
+<div id="isc_9" class="loginFormUser" style="position: absolute; left: 0px; top: 90px; width: 100%; heig…; box-sizing: border-box; cursor: default;" >
+
+
+   <div class="example" style="border: 0px;">
+            <div class="grid fluid">
+                <div class="row" style="text-align:center; position:relative;">
+                    <div class="span4">
+                        <div class="notice1">
+                            <div class="fg-grai-fo"><b>CREATE & ACTIVATE YOUR ACCOUNT</b> <br> You will receive an e-mail at the address you provide that contains an account activation link.</div>
+                        </div>
+                    </div>
+                    <div class="span4">
+                        <div class="notice2">
+                            <div class="fg-grai"><b>UPLOAD A FILE</b> <br> Once logged in, click on "upload" from the menu and provide a short file description to help other students in their search.</div>
+                        </div>
+                    </div>
+                    <div class="span4">
+                        <div class="notice3">
+                            <div class="fg-white"><b>ACCESS NOTES, ASSIGNEMENTS AND EXAMS</b> <br>Browse our database and get your hands on academic ressources to help you improve your grades.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                        
+                        
+
+
+    <div id="isc_A" style="POSITION:relative;display:inline-block;-moz-box-sizing:borde…-align:top;VISIBILITY:inherit;Z-INDEX:200126;CURSOR:default;" >
+
+
+</div>
+
+
+    <center>
+    <div id="isc_1Z" style="POSITION:relative;display:inline-block; padding-top: 10%;" >
+        <p class="generic" style="color: #3E4252; font-family: Open Sans,helvetica,arial,sans-serif;font-weight: 400; font-size: 24px;line-height: 1.55em;"></p>
+            <table style="cellspacing=0; width=295px; height=55px; role=presentation">
+                <tbody>
+                    <tr>
+                    
+                        <td style="align=left; valign=center; nowrap=true; padding-top: 70%;padding-bottom: 20px;color: #0000;font-size: 10px;text-align: center; font-family: Ubuntu,Tahoma,sans-serif">Copyright (c) 2015, Archway. All Rights Reserved</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </center>
+</div>
+</div>
+</div>
+
+
+
+
+
+<div id="isc_1X" class="loginBottomLogo" style="position: absolute; top: 70px%; width: 115px; h…; ">
+
+ 
+
+</div>
+
+</div>
+
+        </body>
+    </html>
+
--- original.php
+++ original.php
@@ -23,13 +23,11 @@
 
     </head>
 
-    <body class="metro" style="height: 100%;">
+    <body class="metro">
 
 <script></script>
 
 <noscript></noscript>
-
-<div class="container">
 
 <div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
 
@@ -39,7 +37,13 @@
         
                  
                
+    
+    <div id="isc_4" style="POSITION:relative;display:inline-block;-moz-box-sizing:borde…-align:top;VISIBILITY:inherit;Z-INDEX:200072;CURSOR:default;" >
+        <div id="isc_5" class="normal" style="position: absolute; left: 0px; top: 0px; width: 779px; heigh…; box-sizing: border-box; cursor: default;" >
 
+       </div>
+
+</div>
 
 <div id="isc_9" class="loginFormUser" style="position: absolute; left: 0px; top: 90px; width: 780px; heig…; box-sizing: border-box; cursor: default;" >
 
@@ -60,11 +64,11 @@
                             <form action="search.php" method="post"> 
                             <div class="input-control text">
                             <input type="text" placeholder="Search resources by course or keyword" style="width:80%;"/><button class="active" style="padding: 7px 12px;"><img src="images/search-3071e9e44daa3fd755860cfeb35f83e4.png" width="75%" height="75%;> </button>
+                            </div>
+                            
+                            </form>
                         </div>
-
-                    </form>
-                </div>  
-            </center> 
+                    </center> 
                         
                         
 
@@ -78,12 +82,29 @@
     
     <div id="isc_1Z" style="POSITION:relative;display:inline-block; padding-top: 10%;" >
         <p class="generic" style="color: #606B7C; font-family: Open Sans,helvetica,arial,sans-serif;font-weight: 400; font-size: 24px;line-height: 1.55em;">A free and powerful platform that helps uOttawa students to share courses-related documents.</p>
+            <table style="cellspacing=0; width=295px; height=55px; role=presentation">
+                <tbody>
+                    <tr>
+                    
+                        <td style="align=left; valign=center; nowrap=true; padding-top: 70%;padding-bottom: 20px;color: #0000;font-size: 10px;text-align: center; font-family: Ubuntu,Tahoma,sans-serif">Copyright (c) 2015, Archway. All Rights Reserved</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 </div>
 </div>
 </div>
 
 
+
+
+
+<div id="isc_1X" class="loginBottomLogo" style="position: absolute; top: 70px%; width: 115px; h…; ">
+
+ 
+
 </div>
+
 </div>
 
         </body>
