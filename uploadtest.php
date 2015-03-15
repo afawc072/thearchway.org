8880baeae9fe9e3f731d5afb910a66ef877917f3
pboud <pboud071@uottawa.ca>
2015-03-15 16:12:01 -0400

    Select file is now functionnal on uploadtest.php

--- uploadtest.php
+++ uploadtest.php
@@ -25,8 +25,6 @@
     <script src="js/github.info.js"></script>
     <script src="js/ga.js"></script>
 
-
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <script type="text/javascript">
 
     $(document).ready(function() {
