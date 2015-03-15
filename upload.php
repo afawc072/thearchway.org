48685e86004b5d1d4e8ff7a28f4d8fa3a199a542
pboud071 <pboud071@uottawa.ca>
2015-03-15 19:47:18 -0400

    new file:   scripts/archway1_backup_15_03_2015_16_13_39.sql 	modified:   stats/coursesStats.txt 	modified:   upload.php 	new file:   upload/uploadedFiles/CSI1511/CSI1511_TEST.docx 	new file:   upload/uploadedFiles/CSI1511/CSI1511_TEST2.doc 	new file:   upload/uploadedFiles/CSI1511/CSI1511_TEST2.doc.description 	new file:   upload/uploadedFiles/ELG3125/ELG3125_TEST.docx 	new file:   upload/uploadedFiles/ELG3125/ELG3125_TEST2.doc 	new file:   upload/uploadedFiles/ELG3125/ELG3125_TEST2.doc.description 	new file:   upload/uploadedFiles/MAT1511/MAT1511_TEST.docx 	new file:   upload/uploadedFiles/MAT1511/MAT1511_TEST.docx.description 	renamed:    upload.php -> upload_previous.php

--- upload.php
+++ upload.php
@@ -1,27 +1,49 @@
 <!DOCTYPE html>
-    <html style="">
+    <html>
 
     <head>
 
-<?php session_start() ?>
-    <link href="../css/metro-bootstrap.css" rel="stylesheet">
-    <link href="../css/metro-bootstrap-responsive.css" rel="stylesheet">
-    <link href="../iconFont.css" rel="stylesheet">
-    <link href="../docs.css" rel="stylesheet">
-    <link href="../prettify.css" rel="stylesheet">
+<?php 
+session_start();
+if(!isset($_SESSION['username'])){
+header('Location:/archway/profile.html');
+}
+?>
+    <link href="css/metro-bootstrap.css" rel="stylesheet">
+    <link href="css/metro-bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/iconFont.css" rel="stylesheet">
+    <link href="css/docs.css" rel="stylesheet">
+    <link href="js/prettify/prettify.css" rel="stylesheet">
 
-    <script src="../jquery.min.js"></script>
-    <script src="../jquery.widget.min.js"></script>
-    <script src="../jquery.mousewheel.js"></script>
-    <script src="../prettify.js"></script>
+    <!-- Load JavaScript Libraries -->
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="js/jquery/jquery.widget.min.js"></script>
+    <script src="js/jquery/jquery.mousewheel.js"></script>
+    <script src="js/prettify/prettify.js"></script>
+    <script src="js/holder/holder.js"></script>
 
-    <script src="../load-metro.js"></script>
+    <!-- Metro UI CSS JavaScript plugins -->
+    <script src="js/load-metro.js"></script>
 
-    <script src="../docs.js"></script>
-    <script src="../github.info.js"></script>
-    <script src="../start-screen.js"></script>
-    <script src="../scripts/loading.js"></script>
+    <!-- Local JavaScript -->
+    <script src="js/docs.js"></script>
+    <script src="js/github.info.js"></script>
+    <script src="js/ga.js"></script>
 
+    <script type="text/javascript">
+
+    $(document).ready(function() {
+         $("#detailsgiven").change(function() {
+            if($(this).is(":checked")) {                
+              $("#details").removeAttr("disabled");
+             }
+            else {
+              $("#details").attr("disabled", "disabled");
+             }
+        });
+    
+    });
+    </script>
 
     </head>
 
@@ -30,49 +52,71 @@
 <script></script>
 
 <noscript></noscript>
-
+<?php include ("templates/header.php"); ?>
 <div class="container">
 
-<div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
+        <div class="example" style="padding-top : 20px;">
+            <form action="upload/upload_file.php" method="post" id="uploadform"
+             enctype="multipart/form-data">
+                <fieldset>
+                    <legend>Upload your notes</legend>
+                    <label>File</label>
+                    <div class="input-control file" data-role="input-control">
+                        <input type="file" name="file">
+                        <button class="btn-file"></button>
+                    </div>
+                    <label for="course">Course</label>
+                    <div class="input-control select">
+                        <!--<select name='course'>-->
+                            <?php
 
-<?php include ("../templates/header.php"); ?>
+                                //open connection
+                                $conn = mysql_connect("localhost","admin","vincentdb") or die(mysql_error());
+                                //select database
+                                mysql_select_db("archway1", $conn);
+                                $query = "SELECT * FROM Course ORDER BY cname"; //You don't need a ; like you do in SQL
+                                $result = mysql_query($query);
 
+                                echo "<select name= 'course'>";// start a table tag in the HTML
+                                echo '<option value="">'.'--Select Course--'.'</option>';
+                                while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
+                                    echo "<option value='". $row['cname'] . "'>".$row['cname'] .'</option>';
+                                }
 
+                                echo '</select>';
+                                mysql_close(); //Make sure to close out the database connection
 
+                            ?>
+                            </select>
+                    </div>
+                    
 
-<center><body>
+                    <label>Description</label>
+                    <div class="input-control textarea">
+                        <textarea name="details" id="details" form="uploadform" data-transform="input-control" placeholder="This is optional..."></textarea>
+                     </div>
 
-<form action="upload/upload_file.php" method="post"
-enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="file" id="file"><br>
-<label for="course" >Course:</label>
-<?php
+                        <div class="input-control switch" data-role="input-control">
+                            <label class="inline-block" style="margin-right: 20px">
+                                Add description
+                                <input type="checkbox" name="detailsgiven" id="detailsgiven" checked/>
+                                <span class="check"></span>
+                            </label>
+                        </div>
+                    
+                    <br>
+                    <input type="submit" name="submit" value="Upload">
+                    <input type="reset" value="Reset">
 
-//open connection
-$conn = mysql_connect("localhost","admin","vincentdb") or die(mysql_error());
-//select database
-mysql_select_db("archway1", $conn);
-$query = "SELECT * FROM Course ORDER BY cname"; //You don't need a ; like you do in SQL
-$result = mysql_query($query);
+                    <div style="margin-top: 20px">
+                    </div>
 
-echo "<select name= 'course'>";// start a table tag in the HTML
-echo '<option value="">'.'--Select Course--'.'</option>';
-while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
-echo "<option value='". $row['cname'] . "'>".$row['cname'] .'</option>';
-}
+                </fieldset>
+            </form>
+        </div>
 
-echo '</select>';
-//mysql_close(); //Make sure to close out the database connection
-
-?>
-<input type="submit" name="submit" value="Submit">
-</form>
 
 <a href="upload/add_course.php"> Can't find a course? </a>
 
-<div class="footer">
-&copy; 2014 The Archway
-</div>
 </body></center>
 </html> 
