b408c3a88364fa4f4312f98a3bab443df59816d1
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-03-22 03:35:27 -0400

    Added MYSQL

--- scripts/serviceCheck.sh
+++ scripts/serviceCheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 lighttpd="lighttpd"
-mysql="mysqld"
+mysql="mysql"
 php="php"
 
 if (( $(ps -ef | grep -v grep | grep $lighttpd | wc -l) > 0 ))
@@ -8,4 +8,10 @@ then
 echo "$lighttpd is running!!!"
 else
 /etc/init.d/$lighttpd start
+fi
+if (( $(ps -ef | grep -v grep | grep $mysql | wc -l) > 0 ))
+then
+echo "$mysql is running!!!"
+else
+/etc/init.d/$mysql start
 fi
