5da8a44355597809a0d85663d565fdc233bde665
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-03-22 03:43:23 -0400

    Text and Colours added

--- scripts/serviceCheck.sh
+++ scripts/serviceCheck.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 lighttpd="lighttpd"
 mysql="mysql"
-php="php"
 
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+
+echo "     *******************************************************"
+echo "     *		      		                           *"
+echo "     *				 	                   *"
+echo "     *						           *"
+echo "     *	          - T H E   A R C H W A Y -                *"
+echo "     *		          - Service Check -                *"
+echo "     *				            	           *"
+echo "     *		 	           		           *"
+echo "     *******************************************************"
+
+sleep 5
+echo "Verifying the status of Lighttpd"
+sleep 1
 if (( $(ps -ef | grep -v grep | grep $lighttpd | wc -l) > 0 ))
 then
-echo "$lighttpd is running!!!"
+	echo "$lighttpd $COL_GREEN[OK]$COL_RESET!!!"
 else
-/etc/init.d/$lighttpd start
+	echo "Lighttpd $COL_GREEN[DOWN]$COL_RESET"
+	sleep 1
+	/etc/init.d/$lighttpd start
 fi
+echo "Verifying the status of Mysql"
+sleep 1
 if (( $(ps -ef | grep -v grep | grep $mysql | wc -l) > 0 ))
 then
-echo "$mysql is running!!!"
+	echo "$Mysql $COL_GREEN[OK]$COL_RESET!!!"
 else
-/etc/init.d/$mysql start
+	echo "Mysql $COL_RED[OK]$COL_RESET"
+	sleep 1
+	/etc/init.d/$mysql start
 fi
