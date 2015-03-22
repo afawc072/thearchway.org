011da0b76de7193af1087f59512958caea80634c
pboud <pboud071@uottawa.ca>
2015-03-22 08:35:53 -0400

    Mods on help

--- about.php
+++ about.php
@@ -29,26 +29,22 @@
 
 <noscript></noscript>
 
-<div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
+<div class="container" style="min-height: 100%; height: auto !important; height: 100%; margin: 0 auto -100px;">
 
-<?php include ("templates/header.php"); ?>
+    <div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
 
-    <div id="isc_3" class="normal" onscroll="return isc_VLayout_0._handleCSSScroll()" style="position: relative;width: 100%; top: 10%; margin: 0 auto;">
+    <?php include ("templates/header.php"); ?>
+
+    <div class="container" style="margin-top: 50px;">
+
         
-                 
-               
-    
-    <div id="isc_4" style="POSITION:relative;display:inline-block;-moz-box-sizing:borde…-align:top;VISIBILITY:inherit;Z-INDEX:200072;CURSOR:default;" >
-        <div id="isc_5" class="normal" style="position: absolute; left: 0px; top: 0px; width: 779px; heigh…; box-sizing: border-box; cursor: default;" >
-
-       </div>
-
-</div>
-
-<div id="isc_9" class="loginFormUser" style="position: absolute; left: 0px; top: 90px; width: 100%; heig…; box-sizing: border-box; cursor: default;" >
+       
+ <div id="isc_3" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px;width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
+ 
+ <p class="generic" style="color: #3E4252;font-weight: 600;font-family: Segoe UI_,Open Sans,Verdana,Arial,Helvetica,sans-serif;font-weight: 400; font-size: 24px;line-height: 1.55em; margin-left: 57px;"><b>Help /</b> The Basics</p>
 
 
-   <div class="example" style="border: 0px;">
+   <div class="example" style="border: 0px; witdh: 100%;">
                 <div class="grid fluid">
                 <div class="row" style="text-align:center; position:relative;">
                     <div class="span4">
@@ -87,40 +83,22 @@
                         
 
 
-    <div id="isc_A" style="POSITION:relative;display:inline-block;-moz-box-sizing:borde…-align:top;VISIBILITY:inherit;Z-INDEX:200126;CURSOR:default;" >
+
+
 
 
 </div>
-
-
-    <center>
-    <div id="isc_1Z" style="POSITION:relative;display:inline-block; padding-top: 10%;" >
-        <p class="generic" style="color: #3E4252; font-family: Open Sans,helvetica,arial,sans-serif;font-weight: 400; font-size: 24px;line-height: 1.55em;"></p>
-            <table style="cellspacing=0; width=295px; height=55px; role=presentation">
-                <tbody>
-                    <tr>
-                    
-                        <td style="align=left; valign=center; nowrap=true; padding-top: 70%;padding-bottom: 20px;color: #0000;font-size: 10px;text-align: center; font-family: Ubuntu,Tahoma,sans-serif">Copyright (c) 2015, Archway. All Rights Reserved</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </center>
 </div>
 </div>
 </div>
 
+<?php include ("templates/footer.php"); ?>
 
 
-
-
-<div id="isc_1X" class="loginBottomLogo" style="position: absolute; top: 70px%; width: 115px; h…; ">
 
  
 
-</div>
 
-</div>
 
         </body>
     </html>
