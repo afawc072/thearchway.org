506dc923a35c5be3237f3460060d9082c259086c
pboud <pboud071@uottawa.ca>
2015-03-23 20:56:03 -0400

    Footer fix attempt (min-height for all pages are now 950px)

--- upload.php
+++ upload.php
@@ -107,7 +107,7 @@ header('Location:/archway/profile.html');
 
 <noscript></noscript>
 
-<div class="container" style="min-height: 100%; height: auto !important; height: 100%; margin: 0 auto -100px;">
+<div class="container" style="min-height: 950px; height: auto !important; margin: 0 auto -100px;">
 
     <div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
 
