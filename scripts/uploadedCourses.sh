2b9699f95e8d4458ba725e660f1d83c9f84830be
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-03-24 22:34:56 -0400

    Updated uploadedCourses… will now check the number of elements in Files DB

--- scripts/uploadedCourses.sh
+++ scripts/uploadedCourses.sh
@@ -10,21 +10,9 @@ dirList=$(du -hs)
 
 date=$(date -I) 
 
-ppt=$(locate -c *.ppt)
-
-docx=$(locate -c *.docx)
-
-doc=$(locate -c *.doc)
-
-pdf=$(locate -c *.pdf)
-
-odt=$(locate -c *.odt)
-
-
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!">"$path"
 echo "Statistics Courses with Uploaded Content $date">>"$path"
 echo "There are $numCourses courses containing files">>"$path"
-echo $ppt" Powerpoint, "$docx" DocX files, "$doc" Doc files, "$pdf" PDF files and "$odt" OpenOffice/LibreOffice files">>"$path"
 echo "Total Size: $dirList ">>"$path"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!">>"$path"
 
