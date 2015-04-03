d0e0c5a427f82f0c2ce92d14d0dee0e25e4352ca
pboud <pboud071@uottawa.ca>
2015-04-03 12:24:18 -0400

    Exact course input behavior added in search2.php

--- search2.php
+++ search2.php
@@ -52,15 +52,74 @@ header('Location:/archway/profile.html');
                     $input = mysql_real_escape_string( $input );
                     $input = trim( $input );
 
+                    //Querying for all courses that are LIKE input
                     $sql1 = "SELECT cname FROM Course WHERE cname  LIKE '%$input%';";
                     $data = mysql_query($sql1, $conn) or die(mysql_error());
                     $result = mysql_fetch_array($data);
                     $anymatches=mysql_num_rows($data);
 
+
+                    /****************************************
+                    *                 [IF]                  *
+                    *       Input is a EXACT course         *
+                    *   name, return the files uploaded     *
+                    *         in its specific folder        *
+                    *                                       *
+                    *****************************************/
                     if(preg_match('/([A-Za-z]{3})([0-9]{4})/', $input)){
 
-                    } 
 
+                    echo'<div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">';
+                    include ("templates/header.php");
+                    echo '<div class="container" style="min-height: 850px;height: auto !important; margin: 0 auto -100px;">';
+                    echo '<div style="padding-bottom: 200px;">';
+                    echo '<div class="example">';
+
+                        //put the results on the screen
+                        $path = "upload/uploadedFiles/".$input;
+                        $pathtotal= $path."*";
+                        // echo "<br><b>$title</b>";
+                        // echo "<br>$info<br>";
+                        
+
+                        $except = array("doc", "docx", "odt", "ppt", "pdf");
+                        $imp = implode('|', $except);
+
+                        if($path != "." && $path != ".." ){
+                        $thelist="";
+                          if ($handle = opendir($path)) {
+                            while (false !== ($file = readdir($handle))) {
+
+                              if ($file != "." && $file != ".." && (preg_match('/^.*\.('.$imp.')$/i', $file)))
+                               {
+                                $tempp= $path."/".$file;
+                                if (file_exists($tempp.".description")){
+                                    $thelist .= '<li><a href="'.$tempp.'">'.$file.'</a></li>';
+                                    $thelist .= file_get_contents($tempp.".description");
+                                    $thelist .= '<br> </br>';
+                                   }
+                                else{
+                                  $thelist .= '<li><a href="'.$tempp.'">'.$file.'</a></li>';
+                                  $thelist .= '<br> </br>';
+                                    }
+
+                              }
+                            }
+                            closedir($handle);
+                          }
+
+
+                        }
+                        echo "List of files for ".$input.":";
+                        echo $thelist;
+                    }
+
+                    /****************************************
+                    *               [ELSE IF]               *
+                    *     Input is found within courses     *
+                    *      names, return these courses      *
+                    *                                       *
+                    *****************************************/
                     else if (preg_match('/^[A-Za-z0-9]+$/', $input) &&  $anymatches!=0) {
 
                     echo'<div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">';
@@ -82,42 +141,41 @@ header('Location:/archway/profile.html');
 
                         while ($courseFetcher = mysql_fetch_array($data)) {
 
-
-
                                 $courseList .= '<li><a href="">'.$courseFetcher['cname'].'</a></li><br>';
                                 echo '<tr class=""><td>'.$courseFetcher['cname'].'</td></tr>';
-                            
-                            
+                                          
                             }
                             mysql_close($conn);
 
                                 echo'</tbody>';
                                 echo '</table>';
+                                echo '</div>';
                             }
+
+                    /****************************************
+                    *                 [ELSE]                *
+                    *     Return all files that match       *
+                    *               the input               *
+                    *                                       *
+                    *****************************************/
                             else
                             {
                                 echo '<div class="container" style="min-height: 950px; height: auto !important; margin: 0 auto -100px;">';
-
                                 echo '<div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">';
 
                                 include ("templates/header.php");
 
                                 echo '<div class="container" style="margin-top: 50px;">';
-
                                 echo "<p class='generic' style='color: #3E4252;font-weight: 600;font-family: Segoe UI_,Open Sans,Verdana,Arial,Helvetica,sans-serif;font-weight: 400; font-size: 24px;line-height: 1.55em;'><b>Search /</b> No results</p>";
-
-
                                 echo "<p style='color: #606B7C; font-family: Sergoe UI Light_, Open Sans Light, Verdana, Arial, Helvetica, sans-serif;font-weight: 400; font-size: 18px;line-height: 1.55em;'>Unfortunately, <span style='color: #00A255;'>$input</span> did not return any document or course. For tips on how to find the ressources you are looking for, </p>";
-                        
-                   
-
+                                echo ' </div>';
                             }
                             ?>
-            </div>
+        
         </div>
-            </div>
-            <?php include ("templates/footer.php"); ?>
         </div>
+        <?php include ("templates/footer.php"); ?>
+    </div>
     </div>
 
 </body></center>
