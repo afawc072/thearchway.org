c032427a33460533e9f6e0e559bb64c321aafd2f
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 11:55:07 -0400

    Db Connect problem with login

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,6 @@ include("variables/dbconnect.inc.php");
 
  // Connects to Our Database 
  $con = mysql_connect($host, $dbuser, $dbpass)or die(mysql_error()); 
- mysql_select_db($dbname , $con);
- $result = mysql_query("SELECT * FROM Users");
-var_dump($result);
+ mysql_select_db($dbname,$con);
+
  ?> 
