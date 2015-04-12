84cf58d6bf1aab827fa4402563b481d6c9fd6f91
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 12:42:21 -0400

    Encryption

--- variables/encryption.php
+++ variables/encryption.php
@@ -1,12 +1,14 @@
 <?php
-
-include("variables/variables.inc");
+include("variables/variables.inc.php");
 
 function encrypt($pass){
 
-$pass_hash1 = md5($pass . $salt);	
+$pass_hash1 = md5($pass . $salt1);
+echo $pass_hash1;
+$passFinal = sha1($salt2 . $pass_hash1);
+echo $passFinal;
 
-
+return $passFinal;
 
 }
 
