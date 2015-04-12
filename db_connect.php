ab6b08c7c304707fae0194bdc31deb71037241b4
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 12:10:56 -0400

    db_connect try 2

--- db_connect.php
+++ db_connect.php
@@ -6,4 +6,13 @@ require "variables/dbconnect.inc.php";
  $con = mysql_connect($host, $dbuser, $dbpass)or die(mysql_error()); 
  mysql_select_db($dbname,$con);
 
+$con = mysql_connect("localhost","admin","vincentdb")or die(mysql_error());
+		
+if (!$con)
+ 	  {
+ 	  echo"No Connection";
+	  die('Could not connect: ' . mysql_error());
+ 	  }
+		 
+ 	mysql_select_db("archway1", $con); 
  ?> 
