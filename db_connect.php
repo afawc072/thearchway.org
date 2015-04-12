9017cc0007ae25ff2414a9a6c3650575360dcec7
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 11:41:28 -0400

    db connect trouble shoot

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,8 @@
 include("variables/dbconnect.inc.php");
 
  // Connects to Our Database 
- $conn=mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
- mysql_select_db($dbname,$conn) or die(mysql_error()); 
+ mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
+ mysql_select_db($dbname) or die(mysql_error()); 
+ $result = mysql_query("SELECT * FROM Users WHERE email = '$email' AND password = '$password' ");
+echo $result;
  ?> 
