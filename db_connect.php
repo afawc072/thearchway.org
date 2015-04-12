95b35679beb223dc75b32d46d31c7dec214c9c42
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 11:40:23 -0400

    DB connect

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,6 @@
 include("variables/dbconnect.inc.php");
 
  // Connects to Our Database 
- mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
- mysql_select_db($dbname) or die(mysql_error()); 
+ $conn=mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
+ mysql_select_db($dbname,$conn) or die(mysql_error()); 
  ?> 
