cc4c928f3039f2980d1a79a97e5a0d9244173332
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 11:38:52 -0400

    DB Conencts should work

--- db_connect.php
+++ db_connect.php
@@ -2,12 +2,6 @@
 include("variables/dbconnect.inc.php");
 
  // Connects to Our Database 
- $con=mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
-
-		if (!$con)
-		  {
-		  echo"No Connection";
-		  die('Could not connect: ' . mysql_error());
-		  }
+ mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
  mysql_select_db($dbname) or die(mysql_error()); 
  ?> 
