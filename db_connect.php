820c7f31bc30cc4f90f1d1e72c97f4f68db17971
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 11:44:24 -0400

    tb 3

--- db_connect.php
+++ db_connect.php
@@ -4,6 +4,6 @@ include("variables/dbconnect.inc.php");
  // Connects to Our Database 
  mysql_connect($host, $dbuser, $dbpass) or die(mysql_error()); 
  mysql_select_db($dbname);
- $result = mysql_query("SELECT * FROM Users WHERE email = '$email' AND password = '$password' ");
+ $result = mysql_query("SELECT * FROM Users");
 var_dump($results);
  ?> 
