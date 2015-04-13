333ab5ea709e16eaa66672e937162125700a3ccf
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 20:41:08 -0400

    Cleaned UploadedFiles .description files

--- scripts/db_files_update.sh
+++ scripts/db_files_update.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
-mysql -u admin -pvincentdb << EOF
-use archway1;
-select * from Files;
-EOF
+while read -a row
+do
+
+echo ${row[1]}
+if [ "${row[1]}" == "path" ]
+then
+echo "row path"
+
+elif [ -f ${row[1]} ]
+then
+echo "file exists"
+
+else
+echo "file doesn't exists, therefore will be deleted from DB"
+echo "use archway1;DELETE from Files where id like '${row[0]}';" | mysql -u root -pvince$
+
+fi
+
+done < <(echo "use archway1;SELECT id,path from Files" | mysql -u admin -pvincentdb)
