be687983746d8474712a8abcf30a7c87aeb69d5b
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-12 21:28:17 -0400

    File Update to add to DB

--- scripts/db_files_update.sh
+++ scripts/db_files_update.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+echo "WE WILL NOW UPDATE THE DB BY REMOVING FROM IT FILES THAT AREN'T IN THE FILE SERVER ANYMORE"
+sleep 5
+
 while read -a row
 do
 
@@ -13,8 +17,20 @@ echo "file exists"
 
 else
 echo "file doesn't exists, therefore will be deleted from DB"
-echo "use archway1;DELETE from Files where id like '${row[0]}';" | mysql -u root -pvince$
+echo "use archway1;DELETE from Files where id like '${row[0]}';" | mysql -u admin -pvincentdb
 
 fi
 
 done < <(echo "use archway1;SELECT id,path from Files" | mysql -u admin -pvincentdb)
+
+echo "WE WILL NOW UPDATE THE DB WITH COURSES THAT MIGHT HAVE SLIPPED THROUGH THE CRACKS OF OUR DB OR WERE ADDED MANUALLY TO THE FILE SERVER"
+sleep 5
+
+ARRAY=( $(find /var/www/archway/upload/uploadedFiles/* | grep -e ".doc" -e ".odt" -e ".ppt" -e ".docx" -e ".pdf") )
+
+for i in "${ARRAY[@]}"
+	do
+	:
+	COURSENAME=$(echo ${ARRAY[i]} | cut -c47-)
+done
+
