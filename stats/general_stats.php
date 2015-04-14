b9e57305a5406938f905a583abc22c42cb09f2ec
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-13 22:22:34 -0400

    generalstats typo

--- stats/general_stats.php
+++ stats/general_stats.php
@@ -3,7 +3,7 @@
 require "db_connect.php";
 
 
-result = mysql_query("select user, count(*) as occurences from Files group by user order by occurences desc, user limit 3;");
+$result = mysql_query("select user, count(*) as occurences from Files group by user order by occurences desc, user limit 3;");
 
 while($row = mysql_fetch_array($result)){
 	echo $row
