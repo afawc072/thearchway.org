81d337edd9f05d2d6ed07372935a5e1a57df37cf
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-04-13 22:21:25 -0400

    General Stats... i.e: Best Users

---
+++ stats/general_stats.php
@@ -0,0 +1,12 @@
+<?php
+
+require "db_connect.php";
+
+
+result = mysql_query("select user, count(*) as occurences from Files group by user order by occurences desc, user limit 3;");
+
+while($row = mysql_fetch_array($result)){
+	echo $row
+}
+
+?>
