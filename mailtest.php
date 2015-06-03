9ba5c497bb2cd8356e21386aa4d9d2647e54e262
pboud <pboud071@uottawa.ca>
2015-06-03 13:02:32 -0400

    PHPMailer

---
+++ mailtest.php
@@ -0,0 +1,45 @@
+<?php
+    require_once('class.phpmailer.php');
+
+    include("sendmail.php");
+    $to       =   "pierluc.boudreau@gmail.com";
+    $subject  =   "Archway test email";
+    $message  =   "hello <i>this is a test from Luke.</i>";
+    $$name     =   "Shahid Shaikh";
+    $$mailsend =   sendmail($to,$subject,$message,$name);
+      if($mailsend==1){
+        echo '<h2>email sent.</h2>';
+      }
+      else{
+        echo '<h2>There are some issue.</h2>';
+      }
+
+
+
+
+    function sendmail($to,$subject,$message,$name)
+    {
+                  $mail             = new PHPMailer();
+                  $body             = $message;
+                  $mail->IsSMTP();
+                  $mail->SMTPAuth   = true;
+                  $mail->Host       = "thearchwayorg.domain.com";
+                  $mail->Port       = 587;
+                  $mail->Username   = "noreply@thearchway.org";
+                  $mail->Password   = "Get!No58?reply";
+                  $mail->SMTPSecure = 'tls';
+                  $mail->SetFrom('noreply@thearchway.org', 'The Archway');
+                  $mail->AddReplyTo("noreply@thearchway.org","The Archway");
+                  $mail->Subject    = $subject;
+                  $mail->AltBody    = "Any message.";
+                  $mail->MsgHTML($body);
+                  $address = $to;
+                  $mail->AddAddress($address, $name);
+                  if(!$mail->Send()) {
+                      return 0;
+                  } else {
+                        return 1;
+                 }
+    }
+?>
+
