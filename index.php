3902ce4927a9a9dffb85336ea002f2faf58ba94d
pboud <pboud071@uottawa.ca>
2015-06-28 14:15:38 -0400

    Mobile testing page - mobiletest.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
     <html style="">
 
     <head>
-    <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php session_start() ?>
 
 <?php
---
+++ mobiletest.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+    <html style="">
+
+    <head>
+    	    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php session_start() ?>
+
+<?php
+header('Cache-Control: max-age=900');
+?>
+    <link href="css/metro-bootstrap.css" rel="stylesheet">
+    <link href="css/metro-bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/iconFont.css" rel="stylesheet">
+    <link href="css/docs.css" rel="stylesheet">
+    <link href="js/prettify.css" rel="stylesheet">
+
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="js/jquery/jquery.widget.min.js"></script>
+    <script src="js/jquery/jquery.mousewheel.js"></script>
+    <script src="js/prettify.js"></script>
+
+    <script src="js/load-metro.js"></script>
+
+    <script src="js/autocomplete/jquery-ui.js"></script>
+    <script src="js/autocomplete/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="js/autocomplete/jquery-ui.css">
+    <link rel="stylesheet" href="js/autocomplete/jquery-ui.min.css">
+
+    <script src="js/docs.js"></script>
+    <script src="js/github.info.js"></script>
+    <script src="js/start-screen.js"></script>
+    <script src="js/ajax_login.js"></script>
+<script type="text/javascript">
+    
+$(document).ready(function() {
+
+
+    $('a.login-window').click(function() {
+        
+        // Getting the variable's value from a link 
+        var loginBox = $(this).attr('href');
+
+        //Fade in the Popup and add close button
+        $(loginBox).fadeIn(300);
+        
+        //Set the center alignment padding + border
+        var popMargTop = ($(loginBox).height() + 24) / 2; 
+        var popMargLeft = ($(loginBox).width() + 24) / 2; 
+        
+        $(loginBox).css({ 
+            'margin-top' : -popMargTop,
+            'margin-left' : -popMargLeft
+        });
+        
+        // Add the mask to body
+        $('body').append('<div id="mask" name="mask"></div>');
+        $('#mask').fadeIn(300);
+        document.getElementById("user").focus();
+        
+        return false;
+    });
+    
+    // When clicking on the button close or the mask layer the popup closed
+    $('a.close, #mask').click( function() { 
+      $('#mask , .login-popup').fadeOut(300 , function() {
+        $('#mask').remove();  
+    }); 
+    return false;
+    });
+
+
+});
+</script>
+
+
+    </head>
+
+    <body class="metro" style="height: 100%;">
+
+        <script></script>
+
+        <noscript></noscript>
+
+            <div class="container" style="min-height: 100%; height: auto !important; margin: 0 auto -100px;">
+
+                <div id="isc_2" class="normal" onscroll="return isc_VLayout_2._handleCSSScroll()" style="position: absolute; left: 0px; top: 0px; width: 100%; heig…cursor: default; display: inline-block; outline-style: none;">
+
+                    <?php include ("templates/header.php"); ?>
+
+        
+                 
+               
+</div>
+</div>
+
+</div>
+
+
+<?php include ("templates/footer.php"); ?>
+
+</div>
+</div>
+        </body>
+    </html>
+
+
