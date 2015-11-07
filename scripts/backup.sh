aa1d67ed2a55afa37ae86b6b35bc73e4b3317cb0
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-11-07 00:31:27 -0500

    Path Modification

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,7 @@
 #This executable script allows you to backup a file and create an archive for it
 #
 #
-#i.e /var/www/archway/scripts/backup.sh archway /var/www/archway
+#i.e /var/www/archway/scripts/backup.sh archway /var/www/
 #
 #
 
