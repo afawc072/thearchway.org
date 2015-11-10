d813f9711e34064c6b987d268ec5d4754fc6f624
Alexandre Fawcett <alex@Alexandres-MacBook-Pro.local>
2015-11-09 22:00:38 -0500

    UTF Decode

--- download.php
+++ download.php
@@ -11,13 +11,14 @@ header('Location:/sign-up.php');
 ignore_user_abort(true);
 set_time_limit(0); // disable the time limit for this script
 
-$dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\].]|[\.]{2,})", '', $_GET['download_file']); // simple file name validation
+//$dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\].]|[\.]{2,})", '', $_GET['download_file']); // simple file name validation
 $courseName = substr($dl_file, 0, 7);
 $path = "/var/www/upload/uploadedFiles/".$courseName."/"; // change the path to fit your websites document structure
 //$dl_file = filter_var($dl_file, FILTER_SANITIZE_URL); // Remove (more) invalid characters
 $path_parts = pathinfo($_GET['download_file']);
 $file_name  = $path_parts['basename'];
 $dl_file  =  $file_name;
+$dl_file = utf8_encode($dl_file);
 $fullPath = $path.$dl_file;
 
 $user = $_SESSION['email'];
